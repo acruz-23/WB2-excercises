e90a9254809efe6c68600e2d066cdc6a93dbc750
acruz-23 <cruz.581@outlook.com>
2023-04-26 09:02:03 -0400

    Created new project folder for basic JS conversion scripts, added script to convert strings into numbers.

---
+++ NumericConversions/conversion_tests.js
@@ -0,0 +1,26 @@
+//var declaration
+var a,b,c,d;
+ 
+//sample data
+a = "  101.1  "
+b ="55";
+c = "402 Stevens"
+d = "Number 5 "
+
+//calc
+console.log("int: ", parseInt(a))
+console.log("int: ", parseInt(b))
+console.log("int: ", parseInt(c))
+console.log("int: ", parseInt(d))
+console.log("Float: ", parseFloat(a))
+console.log("Float: ", parseFloat(b))
+console.log("Float: ", parseFloat(c))
+console.log("Float: ", parseFloat(d))
+console.log("Number: ", Number(a))
+console.log("Number: ", Number(b))
+console.log("Number: ", Number(c))
+console.log("Number: ", Number(d))
+console.log("Unary: ", +a)
+console.log("Unary: ", +b)
+console.log("Unary: ", +c)
+console.log("Unary ", +d)
